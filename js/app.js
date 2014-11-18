c9b8c02a6cb0707e5b7823ec92a5f9fc84316ca3
Jared Jones <jfox1126@gmail.com>
2014-11-18 10:15:10 -0800

    absolute feedback enabled

--- js/app.js
+++ js/app.js
@@ -4,14 +4,54 @@ $(document).ready(function(){
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
-
   	});
-
-  	/*--- Hide information modal box ---*/
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	//Pick a random number b/w 1 and 100
+  	var actualNum = Math.floor((Math.random()*100)+1);
+  	console.log(actualNum);
+
+  	var measure = function () {
+  		var guess = $('#userGuess').val();
+  			console.log(guess);
+  		var distance = Math.abs(guess - actualNum);
+  			console.log(distance);
+  		var feedback
+  		if (distance === 0) {
+	  		feedback = "Got it!";
+	  	} else if (distance <= 5) {
+	  		feedback = "Very Hot!";
+	  	} else if (distance <= 10) {
+	  		feedback = ("Hot!");
+	  	} else if (distance <= 20) {
+	  		feedback = "Lukewarm";
+	  	} else if (distance <= 30) {
+	  		feedback = "Cold";
+	  	} else if (distance > 30) {
+	  		feedback = "Ice Cold";
+	  	};
+	  	$('#feedback').text(feedback);
+	  	$('#guessList').prepend('<li class = entry>'+guess+" - "+feedback+'</li>');
+	  	$('#userGuess').val('');
+  	};
+  	var counter = 0;
+  	$('#guessButton').click(function(event) {
+  		counter ++;
+  		measure();
+  		$('#count').text(counter);
+  		event.preventDefault();
+  	});
+  	$('.new').click (function() {
+  		actualNum = Math.floor((Math.random()*100)+1);
+  		console.log(actualNum);
+  		$('#count').text("0");
+  		$('#userGuess').val('');
+  		$('#feedback').text('Make your Guess!');
+  		counter = 0;
+  		$('.entry').remove();
+  	});
 });
 
 
